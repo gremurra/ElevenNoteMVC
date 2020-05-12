7de5889aee5accc7fea186d71e2dd110b2478f4c
Greg Murray <gremurra@gmail.com>
2020-05-12 16:00:48 -0400

    NoteListItem Model and View

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -18,7 +18,7 @@ namespace ElevenNote.Data
         [Required]
         public string Content { get; set; }
         [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
-        public DateTimeOffset ModifiedUtc { get; set; }
+        public DateTimeOffset? CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
