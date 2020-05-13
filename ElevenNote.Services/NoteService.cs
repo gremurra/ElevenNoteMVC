fb03ead43f0a852c45ad405591e9c5d693fd0b7b
Greg Murray <gremurra@gmail.com>
2020-05-13 10:00:55 -0400

    Added GetNotes and CreateNote in Service Layer

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,60 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;      //creating a private field of type Guid
+
+        public NoteService(Guid userId)     //this is a constructor
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)        //this creates the instance of Note
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()     //allows us to see all the notes that belong to a specific user
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new NoteListItem
+                        {
+                            NoteId = e.NoteId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
